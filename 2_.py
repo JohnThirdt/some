911d4b1025fd0d7f7acf256438f45ce4e754dd5d
JohnThirdt <lvantretiyakov2018@gmail.com>
2024-04-23 15:01:52 +0300

    Deleted

--- 2_.py
+++
@@ -1,9 +0,0 @@
-import jwt
-
-from common import something, secret
-
-data_encoded = jwt.encode(something, secret)
-print(data_encoded, jwt.decode(
-    data_encoded,
-    secret, 
-    algorithms=['HS256']))
